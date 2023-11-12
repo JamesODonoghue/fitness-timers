de7b47b1356ed5e52421113dc6b6e3d0decb35ac
James O'Donoghue <jameso@astra.com>
2023-11-12 10:34:39 -0800

    chore: first commit

---
+++ fitness-timers/fitness_timersApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_timersApp.swift
+//  fitness-timers
+//
+//  Created by James O'Donoghue on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct fitness_timersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
